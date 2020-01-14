f2589b8d4321266e67c597646b29d107bbe9d60a
Zachary Johnson <zach_j@att.net>
2020-01-13 19:48:08 -0700

    first commit

---
+++ server/controllers/books_controller.js
@@ -0,0 +1,40 @@
+const books = []
+let id = 0
+
+module.exports = {
+    read: (req,res) => {
+        res.status(200).send(books)},
+    create: (req,res) => {
+        const {title, author} = req.body
+        let book = {
+            id: id,
+            title: title,
+            author: author
+        }
+        books.push(book)
+        id++
+        req.status(200).send(books)
+    },
+    update: (req,res) => {
+        const {id} = req.params
+        let index = null
+        books.forEach((val, i) => {
+            if (val.id === +id) index=i
+        })
+        books[index] = {
+            id: books[index].id,
+            title: req.body.title || books[index].title,
+            author: req.body.author || books[index].author
+        }
+        res.status(200).send(books)
+    },
+    delete: (req,res) => {
+        const {id} = req.params
+        let index = null
+        books.forEach((val, i) => {
+            if(val.id === +id) index=i
+        })
+        books.splice(index, 1)
+        res.status(200).send(books)
+    }
+}
---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bc = require('./controllers/books_controller.js')
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/api/books', bc.read)
+app.post('/api/books', bc.create)
+app.put('/api/books/:id', bc.update)
+app.delete('/api/books/:id', bc.delete)
+
+
+
+const port = 4000
+app.listen(port, ()=> {
+    console.log(`Server listening on port: ${port}`)
+})
